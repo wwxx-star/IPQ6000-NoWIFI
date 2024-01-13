fe4c141a4f60003d18212142598eb9cb4ff6ab5d
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2024-01-13 17:47:54 +0800

    Update diy-part2-ipq6000.sh

--- diy-part2-ipq6000.sh
+++ diy-part2-ipq6000.sh
@@ -11,7 +11,7 @@
 #
 # git clone  https://github.com/rufengsuixing/luci-app-adguardhome  package/luci-app-adguardhome
 # #git clone https://github.com/muink/openwrt-packages  package/openwrt-packages
-# #git clone https://github.com/wwxx-star/openwrt-packages1  package/openwrt-packages
+git clone https://github.com/wwxx-star/openwrt-packages1  package/openwrt-packages
 # #git clone https://github.com/yfdoor/OpenWrt-Packages.git package/yfdoorg
 # git clone https://github.com/haiibo/openwrt-packages package/haiibo
 # #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
